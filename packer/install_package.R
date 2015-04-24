556ae4b7d0bb28d378aa5024806efaf0adff9ac2
Kiwamu Okabe <kiwamu@debian.or.jp>
2015-04-24 11:36:14 +0900

    Install R library when running make setup

---
+++ packer/install_package.R
@@ -0,0 +1,3 @@
+install.packages("xts", repos = "http://cran.us.r-project.org", dependencies = TRUE)
+install.packages("foreach", repos = "http://cran.us.r-project.org", dependencies = TRUE)
+install.packages("doParallel", repos = "http://cran.us.r-project.org", dependencies = TRUE)

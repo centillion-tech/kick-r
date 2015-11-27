e442d2ab512aba878dee90d4ba5af6c8e27459f8
Kiwamu Okabe <kiwamu@debian.or.jp>
2015-11-28 08:27:59 +0900

    Old ubuntu doesn't have some packages

--- packer/install_package.R
+++ packer/install_package.R
@@ -1,1 +1,2 @@
-#install.packages("xts", repos = "http://cran.us.r-project.org", dependencies = TRUE)
+install.packages("doParallel", repos = "http://cran.us.r-project.org", dependencies = TRUE)
+install.packages("kernlab", repos = "http://cran.us.r-project.org", dependencies = TRUE)

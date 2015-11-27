bf26793a164b41c1d9d73fcbe30716e0954c7d55
Kiwamu Okabe <kiwamu@debian.or.jp>
2015-11-28 08:00:22 +0900

    Use r-cran-* package

--- packer/install_package.R
+++ packer/install_package.R
@@ -1,3 +1,1 @@
-install.packages("xts", repos = "http://cran.us.r-project.org", dependencies = TRUE)
-install.packages("foreach", repos = "http://cran.us.r-project.org", dependencies = TRUE)
-install.packages("doParallel", repos = "http://cran.us.r-project.org", dependencies = TRUE)
+#install.packages("xts", repos = "http://cran.us.r-project.org", dependencies = TRUE)
